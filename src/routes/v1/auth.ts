b928819c7db6fa0f14a63b0b53028af9e7a8a874
Peliah <pelepoupa@gmail.com>
2025-07-16 19:29:30 +0100

    Update rules of engagement for the user registeration

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import register from "@/controllers/v1/auth/register";
-
+import { body } from "express-validator";
+import User from "@/models/user";
 
 const router = Router();
 
-router.post('/register', register);
+router.post(
+    '/register',
+    body('email').trim().notEmpty().withMessage('Email is required').isEmail().withMessage('Invalid email format').custom(async (value) => {
+        const existingUser = await User.exists({ email: value });
+        if (existingUser) {
+            throw new Error('Email already in use');
+        }
+        return true;
+    }),
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long').notEmpty().withMessage('Password is required'),
+    body('role').optional().isIn(['user', 'admin']).isString().withMessage('Role must be either user or admin'),
+    register
+);
 
 export default router;
